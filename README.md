679643a5781aa16e220d61f2c29b24f756164159
Dor Katzir <katzirdor@gmail.com>
2024-07-21 22:49:05 +0300

    project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Laravel-Interia-React
---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="title text-green-500">Hello from Interia and React</h1>
+        </>
+    )
+}
