d01bf0916c3893f4bec3fb989a8122680f391441
Dor Katzir <katzirdor@gmail.com>
2024-07-22 10:44:28 +0300

    Scroll preservation

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Link } from "@inertiajs/react";
 
 export default function Home({name}) {
     return (
         <>
             <h1 className="title text-green-500">Hello {name} </h1>
+
+            <Link preserveScroll className="block title mt-[1000px]" href="">
+                {new Date().toLocaleTimeString()}
+            </Link>
         </>
     )
 }
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,6 +6,7 @@ import { createRoot } from 'react-dom/client'
 import Layout from '@/Layouts/Layout'
 
 
+
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
