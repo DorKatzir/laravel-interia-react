8ab12578baf7d446a6f3e349d742fe9c390c9da1
Dor Katzir <katzirdor@gmail.com>
2024-07-23 20:48:21 +0300

    Pagination

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,12 +3,15 @@ import { Link } from "@inertiajs/react";
 export default function Home({ posts }) {
 
     // console.log(posts)
+    // console.log(posts.data)
+    // console.log(posts.links)
+
     return (
         <>
-            <h1 className="title text-green-500">Posts</h1>
+            <h1 className="title">Posts</h1>
             <div>
                 {
-                    posts.map(post => (
+                    posts.data.map(post => (
                         <div key={post.id} className="p-4 border-b">
                             <div className="text-sm text-slate-600">
                                 <span>Posted on: </span>
@@ -18,6 +21,29 @@ export default function Home({ posts }) {
                                 {post.body}
                             </p>
                         </div>
+                    ))
+                }
+            </div>
+
+            <div className="py-12 px-4">
+
+                {
+                    posts.links.map( link => (
+                        // (console.log(link)),
+                        link.url
+                            ?
+                                <Link
+                                    key={link.label}
+                                    href={link.url}
+                                    dangerouslySetInnerHTML={{ __html: link.label }}
+                                    className={`p-1 mx-1 ${ link.active ? 'text-blue-500 font-bold' : '' }`}
+                                />
+                            :
+                                <span
+                                    key={link.label}
+                                    dangerouslySetInnerHTML={{ __html: link.label }}
+                                    className="p-1 mx-1 text-slate-300"
+                                ></span>
                     ))
                 }
             </div>
